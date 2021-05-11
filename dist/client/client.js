73adab1ef0405e85b93b2ca38944c776e904246f
tpurdon01 <tpurdon01@qub.ac.uk>
2021-05-11 16:24:32 +0100

    formatting

--- dist/client/client.js
+++ dist/client/client.js
@@ -12,21 +12,19 @@ const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 camera.position.z = 2;
 window.addEventListener('resize', onWindowResize, false);
-
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
     render();
 }
-var animate = function() {
+var animate = function () {
     requestAnimationFrame(animate);
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
     controls.update();
     render();
 };
-
 function render() {
     renderer.render(scene, camera);
 }
--- dist/server/server.js
+++ dist/server/server.js
@@ -1,5 +1,5 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function(mod) {
+var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
@@ -11,9 +11,9 @@ class App {
     constructor(port) {
         this.port = port;
         const app = express_1.default();
-        app.use(express_1.default.static(path_1.default.join(__dirname, '../client')));
-        app.use('/build/three.module.js', express_1.default.static(path_1.default.join(__dirname, '../../node_modules/three/build/three.module.js')));
-        app.use('/jsm/controls/OrbitControls', express_1.default.static(path_1.default.join(__dirname, '../../node_modules/three/examples/jsm/controls/OrbitControls.js')));
+        app.use(express_1.default.static(path_1.default.join(__dirname, "../client")));
+        app.use("/build/three.module.js", express_1.default.static(path_1.default.join(__dirname, "../../node_modules/three/build/three.module.js")));
+        app.use("/jsm/controls/OrbitControls", express_1.default.static(path_1.default.join(__dirname, "../../node_modules/three/examples/jsm/controls/OrbitControls.js")));
         this.server = new http_1.default.Server(app);
     }
     Start() {
